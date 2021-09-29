f0651e42d7ae8bb68d5e84515b62e6f26650d1f4
15115778766 <1394046585@qq.com>
2021-09-29 23:09:52 +0800

    init godesk

--- view/Main.go
+++ view/Main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/lxn/walk"
-	"github.com/lxn/win"
+	. "github.com/lxn/walk/declarative"
 	"view/src/com.jenkin.view/wallpaperstruct"
 )
 
@@ -14,27 +15,35 @@ func main() {
 }
 
 func initUI() {
-	window, _ := walk.NewMainWindow()
+	//var inTE, outTE *walk.TextEdit
 
-	// 设置窗体标题
-	window.SetTitle(`你好世界！`)
+	window := MainWindow{}
 
+	var inTE, outTE *walk.TextEdit
 	// 设置窗体的宽高
-	window.SetWidth(400)
-	window.SetHeight(400)
-
-	// 设置窗体生成在屏幕的正中间
-	// 窗体横坐标 = ( 屏幕宽度 - 窗体宽度 ) / 2
-	// 窗体纵坐标 = ( 屏幕高度 - 窗体高度 ) / 2
-	window.SetX((int(win.GetSystemMetrics(0)) - window.Width()) / 2)
-	window.SetY((int(win.GetSystemMetrics(1)) - window.Height()) / 2)
-
-	// 设置窗体为显示状态（默认：隐藏状态）
-	window.Show()
-
-	// 运行窗体
+	window.MinSize = Size{400, 400}
+	window.Title = "GoDesk!"
+	window.Layout = VBox{}
+	window.Children = []Widget{
+		HSplitter{
+			Children: []Widget{
+				TextEdit{Name: "用户名", AssignTo: &inTE},
+				TextEdit{Name: "密码", AssignTo: &outTE},
+			},
+		},
+		PushButton{
+			Text: "登录",
+			OnClicked: func() {
+				fmt.Println("登录")
+			},
+		},
+	}
+	//// 设置窗体生成在屏幕的正中间
+	//// 窗体横坐标 = ( 屏幕宽度 - 窗体宽度 ) / 2
+	//// 窗体纵坐标 = ( 屏幕高度 - 窗体高度 ) / 2
+	//window.SetX((int(win.GetSystemMetrics(0)) - window.Width()) / 2)
+	//window.SetY((int(win.GetSystemMetrics(1)) - window.Height()) / 2)
 	window.Run()
-
 }
 
 func initOption() wallpaperstruct.Option {
--- wallpaperapi/go.mod
+++ wallpaperapi/go.mod
@@ -1,1 +1,3 @@
-module "wallpaperapi"
+module wallpaperapi
+
+go 1.13
